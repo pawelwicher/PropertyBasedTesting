9bee8e355f2f8dc87c5f7d0d2aef40a96914b08e
Paweł Wicher <pwi@epcon.pl>
2020-04-12 11:55:50 +0200

    Add project files.

---
+++ PropertyBasedTesting/DiamondProperties.fs
@@ -0,0 +1,117 @@
+﻿module DiamondProperties
+
+    open System
+    open FsCheck
+    open FsCheck.Xunit
+
+    type Letters =
+        static member Char() =
+            Arb.Default.Char()
+            |> Arb.filter (fun c -> 'A' <= c && c <= 'Z')
+
+    type DiamondPropertyAttribute() =
+        inherit PropertyAttribute(Arbitrary = [| typeof<Letters> |])
+
+    [<Property>]
+    let ``Diamond is non-empty`` (letter : char) =
+        let actual = Diamond.make letter
+        not (String.IsNullOrWhiteSpace actual)
+
+    let split (x : string) =
+        x.Split([| Environment.NewLine|], StringSplitOptions.None)
+
+    let trim (x : string) = x.Trim()
+
+    [<DiamondProperty>]
+    let ``First row contains A`` (letter: char) =
+        let actual = Diamond.make letter
+
+        let rows = split actual
+        rows |> Seq.head |> trim = "A"
+
+    let leadingSpaces (x : string) =
+        let indexOfNonSpace = x.IndexOfAny [| 'A' .. 'Z' |]
+        x.Substring(0, indexOfNonSpace)
+
+    let trailingSpaces (x : string) =
+        let lastIndexOfNonSpace = x.LastIndexOfAny [| 'A' .. 'Z' |]
+        x.Substring(lastIndexOfNonSpace + 1)
+
+    [<DiamondProperty>]
+    let ``All rows must have a symmetric contour`` (letter : char) =
+        let actual = Diamond.make letter
+    
+        let rows = split actual
+        rows |> Array.forall (fun r -> (leadingSpaces r) = (trailingSpaces r))
+
+    [<DiamondProperty>]
+    let ``Top of figure has correct letters in correct order`` (letter : char) =
+        let actual = Diamond.make letter
+        
+        let expected = ['A' .. letter]
+        let rows = split actual
+        let firstNonWhiteSpaceLetters = 
+            rows
+            |> Seq.take expected.Length
+            |> Seq.map trim
+            |> Seq.map Seq.head
+            |> Seq.toList    
+        expected = firstNonWhiteSpaceLetters
+
+    [<DiamondProperty>]
+    let ``Figure is symmetric around the horizontal axis`` (letter : char) =
+        let actual = Diamond.make letter
+
+        let rows = split actual
+        let topRows =
+            rows
+            |> Seq.takeWhile (fun x -> not (x.Contains(string letter))) 
+            |> Seq.toList
+        let bottomRows =
+            rows
+            |> Seq.skipWhile (fun x -> not (x.Contains(string letter)))
+            |> Seq.skip 1
+            |> Seq.toList
+            |> List.rev
+        topRows = bottomRows
+
+    [<DiamondProperty>]
+    let ``Diamond is as wide as it's high`` (letter : char) =
+        let actual = Diamond.make letter
+
+        let rows = split actual
+        let expected = rows.Length
+        rows |> Array.forall (fun x -> x.Length = expected)
+
+    [<DiamondProperty>]
+    let ``All rows except top and bottom have two identical letters``
+        (letter : char) =
+
+        let actual = Diamond.make letter
+
+        let isTwoIdenticalLetters x =
+            let hasIdenticalLetters = x |> Seq.distinct |> Seq.length = 1
+            let hasTwoLetters = x |> Seq.length = 2
+            hasIdenticalLetters && hasTwoLetters
+        let rows = split actual
+        rows
+        |> Array.filter (fun x -> not (x.Contains("A")))
+        |> Array.map (fun x -> x.Replace(" ", ""))
+        |> Array.forall isTwoIdenticalLetters
+
+    [<DiamondProperty>]
+    let ``Lower left space is a triangle`` (letter : char) =
+        let actual = Diamond.make letter
+
+        let rows = split actual
+        let lowerLeftSpace =
+            rows
+            |> Seq.skipWhile (fun x -> not (x.Contains(string letter)))
+            |> Seq.map leadingSpaces
+            |> Seq.toList
+        let spaceCounts = lowerLeftSpace |> List.map (fun x -> x.Length)
+        let expected =
+            Seq.initInfinite id
+            |> Seq.take spaceCounts.Length
+            |> Seq.toList
+        expected = spaceCounts
